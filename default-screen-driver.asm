b3d7b122ab587d13b00054bc8a0a14cfec540554
Derex Guillaume <gderex8@orange.fr>
2018-04-29 12:54:09 +0200

    Corrected for the new screen ops

--- default-screen-driver.asm
+++ default-screen-driver.asm
@@ -1,15 +1,14 @@
 Disp:	;I0 gives the address in ram where the string is
 		;Disp prints a 16 char string from ram
-	LD Bd, 0x10
+	LD Bd, 0x20
 	LD Be, 0
 DispLoop:	
 	LD Be, [I0]
-	OUT 0, Bd	;Sets the character position, as Bd = 0x1?
-	OUT 0, 0x20
+	OUT 0, Bd	;Sets the character position, as Bd = 0x2?, and prints
 	OUT 0, Be
 	INC I0
 	INC Bd
-	CMP Bd, 0x20
+	CMP Bd, 0x30
 	TSTEQ
 	JPNIF DispLoop
 	RET
